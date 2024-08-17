eaaabc73a87cfa70c9bebaf425f03333008e726c
Nedelcu Bogdan Sebastian <nedelcu.bogdan.sebastian@gmail.com>
2024-08-17 23:40:44 +0300

    Add files via upload

---
+++ User/main.h
@@ -0,0 +1,6 @@
+/* Includes ------------------------------------------------------------------*/
+#include "ch32v30x.h"
+
+
+extern u16 usRegHoldingBuf[100+1];
+extern u8 usRegCoilBuf[64/8+1];    // We have 64 coils
